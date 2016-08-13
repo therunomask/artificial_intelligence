35c2326c17338b0d071b4202006de3d38e803d8a
Markus Noeth <noeth@macprodeckert03.local>
2016-08-13 20:00:18 +0200

    started to declare function computing current activation of a layer

--- brain_classes.cpp
+++ brain_classes.cpp
@@ -1,7 +1,23 @@
 #include "brain_header.h"
+#include <vector>
+#include <deque>
 
 void segment::UpdateCon(std::vector<const cell*> winners ){//increase connectedness
     //of winners, decrease connectedness
     //of all other cells in the segment, disconnect them if
     //connection too weak.
+}
+
+
+std::vector<bool> layer::current_activation(){
+    std::vector<bool>input( p_lower_level->current_prediction());
+        //trigger prediction evaluation of lower levels while optaining input
+        //
+        //missing: propagate expectation to lower level!!
+        //
+    std::vector<bool> activation;//reserve space for length of ColumnList
+    for(auto &pillar : ColumnList){
+        //compute activation of columns in the layer
+        activation.push_back(pillar.feed_input(input));
+    }
 }
